8df476c49f255f0c50f6ec8e2a4053240d7b39a4
Pankaj <Pankaj>
2016-11-07 05:53:10 +0530

    Initial Commit

---
+++ app/src/main/java/com/singh/pankaj/android/popularmovies/PopularMoviesFragment.java
@@ -0,0 +1,22 @@
+package com.singh.pankaj.android.popularmovies;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class PopularMoviesFragment extends Fragment {
+
+    public PopularMoviesFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_popular_movies, container, false);
+    }
+}
---
+++ app/src/main/java/com/singh/pankaj/android/popularmovies/model/MovieContract.java
@@ -0,0 +1,16 @@
+package com.singh.pankaj.android.popularmovies.model;
+
+import android.net.Uri;
+import com.singh.pankaj.android.popularmovies.R;
+
+/**
+ * Created by Pankaj on 11/4/2016.
+ */
+
+public class MovieContract {
+   // public static final String CONTENT_AUTHORITY = getString(R.string.content_authority);
+
+  //  public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    public static final String PATH_MOVIE = "movie";
+}
